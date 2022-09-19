b8bf657146506171f03f88b87eb799ff29916a10
c.mariselvan <mari20041999@gmail.com>
2022-09-19 13:00:49 +0530

    add files

---
+++ main.js
@@ -0,0 +1,19 @@
+var str=prompt('enter any name ');
+console.log('No. of letters in given string');
+var length=str.length;
+console.log(length);
+
+var count=0;
+for(i=0;i<length;i++)
+{
+    if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' )
+    {
+        count++;
+    }
+}
+console.log('No. of vowels in given string');
+console.log(count);
+
+
+
+
